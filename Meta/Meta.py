0e8d31e2be7908e681fc38d9bb5dab3a70e0de85
WillHackett22 <wehacket@bu.edu>
2023-05-15 23:27:25 -0400

    MetaUpdatedToProduceUniqueIonIDs

--- Meta/Meta.py
+++ Meta/Meta.py
@@ -38,18 +38,51 @@ class IonMetaDataTable(DataTable):
             dfMeta.loc[dfMeta['fragment_name']==frag,'IonID']=temp['IonID'][i]
     
     def IonMasterListMaker(self):
-        dfIonMetaMasterTemp=pd.DataFrame(data=None,columns=['neutralmass','glycopeptide','fragment_name','fragment_type'])
+        dfIonMetaMasterTemp=pd.DataFrame(data=None,columns=['neutralmass','glycopeptide','peptide','fragment_name','fragment_type'])
         for f in self.files:
             dfIonTemp=Helper.GSoftCSVRead(f,subset=['glycopeptide','fragment_name'],index=self.index,PSMBool=False)
             MetaTemp=self.IonMetadata(dfIonTemp)
             dfIonMetaMasterTemp=pd.concat([dfIonMetaMasterTemp,MetaTemp],ignore_index=True)
         
+        #reduce to unique ions by glycopeptide
         ix=[i for i,x in enumerate(dfIonMetaMasterTemp.duplicated(['glycopeptide','fragment_name'])) if x==False]
-        dfIonMetaMaster=dfIonMetaMasterTemp.loc[ix,]
-        dfIonMetaMaster['IonID']=range(dfIonMetaMaster.shape[0])
-        dfIonMetaMaster['IonID']=dfIonMetaMaster['IonID']
-        self.GlycanSimplifier(dfIonMetaMaster)
+        dfIonMetaMasterTemp=dfIonMetaMasterTemp.loc[ix,]
+        # reduce to glycans
+        GlyMeta=dfIonMetaMasterTemp.loc[dfIonMetaMasterTemp['fragment_type']=='Glycan']
+        #produce IDs for unique glycnas
+        GlyGroups=GlyMeta.groupby(['fragment_name'])['neutralmass'].apply(list).reset_index()
+        GlyGroups['IonID']=range(GlyGroups.shape[0])
+        GlyGroups=GlyGroups.drop(['neutralmass'],axis=1)
+        #attach ids back to glycans by glycopep
+        GlyMetaIDed=pd.merge(GlyGroups,GlyMeta,on='fragment_name')
+        # reduce to peptide
+        PepMeta=dfIonMetaMasterTemp.loc[dfIonMetaMasterTemp['fragment_type']=='Peptide']
+        # give peptide ions ids
+        PepGroups=PepMeta.groupby(['peptide','fragment_name'])['neutralmass'].apply(list).reset_index()
+        PepGroups['IonID']=range(PepGroups.shape[0])
+        PepGroups=PepGroups.drop(['neutralmass'],axis=1)
+        # make pep ions start after gly
+        PepGroups['IonID']=PepGroups['IonID']+GlyGroups.shape[0]
+        # attach back to peptides by glycopep
+        PepMetaIDed=pd.merge(PepGroups,PepMeta,on=['peptide','fragment_name'])
+        #isolate stub
+        StubMeta=dfIonMetaMasterTemp.loc[dfIonMetaMasterTemp['fragment_type']=='Stub']
+        #stub ion ids
+        StubGroups=StubMeta.groupby(['peptide','fragment_name'])['neutralmass'].apply(list).reset_index()
+        StubGroups['IonID']=range(StubGroups.shape[0])
+        StubGroups=StubGroups.drop(['neutralmass'],axis=1)
+        # start stub ion ids after pep and gly
+        StubGroups['IonID']=StubGroups['IonID']+GlyGroups.shape[0]+PepGroups.shape[0]
+        # reattach stubs
+        StubMetaIDed=pd.merge(StubGroups,StubMeta,on=['peptide','fragment_name'])
+        dfIonMetaMaster=pd.concat([StubMetaIDed,PepMetaIDed,GlyMetaIDed])
+        dfIonMetaMaster.loc[dfIonMetaMaster['fragment_type']=="Glycan",'peptide']='Oxonium'
+        unidx=[i for i,x in enumerate(dfIonMetaMaster.duplicated(['IonID'])) if x==False]
+        dfIonMetaUnique=dfIonMetaMaster.iloc[unidx,].reset_index()
+        dfIonMetaUnique=dfIonMetaUnique.drop(['glycopeptide','index'],axis=1)
+        dfIonMetaUnique=dfIonMetaUnique.set_index('IonID')
         self.dfIonMetaMaster = dfIonMetaMaster
+        self.dfIonMetaUnique = dfIonMetaUnique
         
     def ListWriter(self):
         self.dfIonMetaMaster.to_hdf(self.h5file,key=self.key,mode='a')
